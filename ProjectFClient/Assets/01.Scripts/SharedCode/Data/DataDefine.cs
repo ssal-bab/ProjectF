beb9c70f3d3a8b13cced853dd7db2bf475b6bbf7
장하원 <cocoacake84@gmail.com>
2025-02-16 22:36:21 +0900

    add data

--- ProjectFClient/Assets/01.Scripts/SharedCode/Data/DataDefine.cs
+++ ProjectFClient/Assets/01.Scripts/SharedCode/Data/DataDefine.cs
@@ -5,6 +5,9 @@ namespace ProjectF.Datas
         public const int IQ_STAT_MIN = 40;
         public const int IQ_STAT_MAX = 200;
 
+        public const float Farming_Level_Sales_Multiplier = 1.2f;
+        public const float Adventure_Level_Sales_Multiplier = 1.4f;
+
         public const string NO_USER_ID = "None";
     }
 }
--- ProjectFClient/Assets/01.Scripts/SharedCode/Data/User/UserQuestData.cs
+++ ProjectFClient/Assets/01.Scripts/SharedCode/Data/User/UserQuestData.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Collections;
+using System;
 
 namespace ProjectF.Datas
 {
